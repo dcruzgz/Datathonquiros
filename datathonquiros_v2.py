8764cf524437146023bce3af97a591fcc31c1093
Daniel Cruz Garzón <102228842+dcruzgz@users.noreply.github.com>
2022-04-13 15:42:39 +0200

    Update datathonquiros_v2.py

--- datathonquiros_v2.py
+++ datathonquiros_v2.py
@@ -540,7 +540,7 @@ if check_password():
                """)
             st.title(":rocket: Próximas promociones")
 
-            components.html(rules(df_rules()), height=480, width=1000)
+            components.html(rules(df_rules()), height=480, width=800)
             st.write(df_rules().style.format(({"Con una confianza del (%)": "{:.2f}"})))
 
     def run_shell():
