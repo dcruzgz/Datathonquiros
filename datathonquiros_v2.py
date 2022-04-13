9baee25141d4894f9e639e095debd8b6fb68f905
Daniel Cruz Garzón <102228842+dcruzgz@users.noreply.github.com>
2022-04-13 16:00:21 +0200

    Update datathonquiros_v2.py

--- datathonquiros_v2.py
+++ datathonquiros_v2.py
@@ -198,7 +198,7 @@ if check_password():
 
         r_net = Network(height='450px', width='1000px', bgcolor='#FFFFFF.', font_color='black')
         
-        r_net.barnes_hut(spring_length=1, spring_strength=0.08, overlap=1)
+        r_net.barnes_hut(spring_length=100, spring_strength=0.08, overlap=1)
         
         # set the physics layout of the network
 
