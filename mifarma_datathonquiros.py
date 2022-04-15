8b5dfddee71fd038da1cba7989dca4f6b3a61095
Daniel Cruz Garzón <102228842+dcruzgz@users.noreply.github.com>
2022-04-15 19:18:52 +0200

    Update mifarma_datathonquiros.py

--- mifarma_datathonquiros.py
+++ mifarma_datathonquiros.py
@@ -252,13 +252,13 @@ def run_UI():
     
     if st.session_state.page:
         with st.sidebar:
-            image = Image.open('Data/mifarma.png')
+            image = Image.open('mifarma.png')
             st.image(image, caption)
             page = st.radio('Niveles: ', PAGES, index=st.session_state.page)
 
     else:
         with st.sidebar:
-            image = Image.open('Data/mifarma.png')
+            image = Image.open('mifarma.png')
             st.image(image, caption)
             page = st.sidebar.radio('Niveles: ', PAGES, index=0)
 
