d402b29a040f8aa700f17a9a466e8fd26b4d9a6d
Daniel Cruz Garzón <102228842+dcruzgz@users.noreply.github.com>
2022-04-15 17:58:41 +0200

    Update mifarma_datathonquiros.py

--- mifarma_datathonquiros.py
+++ mifarma_datathonquiros.py
@@ -248,9 +248,11 @@ def rules(df_rules):
 def run_UI():
     
     if st.session_state.page:
+         st.sidebar.image('Data/mifarma.png')
         page = st.sidebar.radio('Navigation', PAGES, index=st.session_state.page)
+        
     else:
-        page = st.sidebar.radio('Navigation', PAGES, index=1)
+        page = st.sidebar.radio('Navigation', PAGES, index=0)
 
 #Página MAPA
     if page == 'Nivel geográfico y temporal':
