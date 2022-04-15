50a36d06bb8c6c660d7d3333f2db5ba7b50a4911
Daniel Cruz Garzón <102228842+dcruzgz@users.noreply.github.com>
2022-04-15 15:49:12 +0200

    Update datathonquiros_v2.py

--- datathonquiros_v2.py
+++ datathonquiros_v2.py
@@ -254,10 +254,10 @@ def run_UI():
 #Página MAPA
     if page == 'Nivel geográfico y temporal':
         st.sidebar.write("""
-            ## ## Nivel geográfico y temporal
+            ## Nivel geográfico y temporal
             :pushpin: En esta página puedes visualizar las ventas de Atida Mifarma a nivel provincial. \n
             :calendar: Selecciona las categorías que te resulten de interés, y observa como cambian según el territorio y el mes. \n
-            :moneybag: Si quieres consultar las ganancias ajustadas a los habitantes de cada provincia seleccionar 
+            :people_holding_hands: Si quieres consultar las ganancias ajustadas a los habitantes de cada provincia seleccionar 
             'Ganancias € por 100 mil habitantes' de lo 
             contrario se mostrarán las ganancias totales de cada provincia. \n
             :chart_with_upwards_trend: Más abajo podrás encontrar la evolución temporal de la categoría seleccionada en 
