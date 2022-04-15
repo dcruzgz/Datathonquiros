6a44a471f1aabb09c1e257bd8a7b8078d7fd886f
Daniel Cruz Garzón <102228842+dcruzgz@users.noreply.github.com>
2022-04-15 12:22:04 +0200

    Update datathonquiros_v2.py

--- datathonquiros_v2.py
+++ datathonquiros_v2.py
@@ -27,7 +27,7 @@ st.set_page_config(
 
 @st.experimental_memo(ttl=30)
 def get_data_clean():
-    data = pd.read_csv('https://www.dropbox.com/s/mp1zbrj68ccz0oc/datos_f.csv?dl=1'
+    data = pd.read_csv('https://www.dropbox.com/s/j8792q2ocmc507w/tickets.csv?dl=1'
                          , header=0, encoding="ISO-8859-1")  # read a CSV file inside the 'data" folder next to 'app.py'
     return data
 
