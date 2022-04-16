7bcbea7c9f52348c18f29fd85351e47e8c20e521
Daniel Cruz Garzón <102228842+dcruzgz@users.noreply.github.com>
2022-04-16 13:27:03 +0200

    Update mifarma_datathonquiros.py

--- mifarma_datathonquiros.py
+++ mifarma_datathonquiros.py
@@ -93,24 +93,6 @@ def pretty(s: str) -> str:
         return s.capitalize()
 
 
-if page != 'Nivel de cliente' :
-    #Creación de los DataFrames
-    datos_clean_or = get_data_clean() #General con los datos de los tickets
-    data_code = get_data_prov() #Datos de las provincias (código y población)
-    data_geo = get_data_geo()   #Datos de las coordenadas de las provincias
-
-    # A partir del DataFrame original los NA en categoría de producto pasan a designase 'Sin Clasificar'
-
-    datos_clean_or['productcat1'] = datos_clean_or['productcat1'].fillna('Sin clasificar')
-    datos_clean_or['productcat2'] = datos_clean_or['productcat2'].fillna('Sin clasificar')
-    datos_clean_or['productcat3'] = datos_clean_or['productcat3'].fillna('Sin clasificar')
-
-    # DataFrame donde descartamos los datos sin código postal sólo para la representación del mapa y gráficas de provincias
-    datos_clean_map = datos_clean_or[datos_clean_or['zp_sim'].notna()]
-        
-
-
-
 # region MAPA
 # ---------------------------------
 #     MAPA
@@ -243,8 +225,25 @@ def run_UI():
             image = Image.open('Data/mifarma.png')
             st.image(image)
             page = st.selectbox('Selecciona el nivel de análisis: ', PAGES, index=0)
+            
+    if page != 'Nivel de cliente' :
+    #Creación de los DataFrames
+    datos_clean_or = get_data_clean() #General con los datos de los tickets
+    data_code = get_data_prov() #Datos de las provincias (código y población)
+    data_geo = get_data_geo()   #Datos de las coordenadas de las provincias
 
-#Página MAPA
+    # A partir del DataFrame original los NA en categoría de producto pasan a designase 'Sin Clasificar'
+
+    datos_clean_or['productcat1'] = datos_clean_or['productcat1'].fillna('Sin clasificar')
+    datos_clean_or['productcat2'] = datos_clean_or['productcat2'].fillna('Sin clasificar')
+    datos_clean_or['productcat3'] = datos_clean_or['productcat3'].fillna('Sin clasificar')
+
+    # DataFrame donde descartamos los datos sin código postal sólo para la representación del mapa y gráficas de provincias
+    datos_clean_map = datos_clean_or[datos_clean_or['zp_sim'].notna()]
+        
+
+    #Página MAPA
+    
     if page == 'Nivel geográfico y temporal':
         
         
