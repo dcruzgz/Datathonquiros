fd0db3dc6b74cf23df3edca853cbbba375208949
Daniel Cruz Garzón <102228842+dcruzgz@users.noreply.github.com>
2022-04-16 13:40:52 +0200

    Update mifarma_datathonquiros.py

--- mifarma_datathonquiros.py
+++ mifarma_datathonquiros.py
@@ -98,37 +98,6 @@ def pretty(s: str) -> str:
 #     MAPA
 # ---------------------------------
 
-#Creación de los DataFrames
-datos_clean_or = get_data_clean() #General con los datos de los tickets
-data_code = get_data_prov() #Datos de las provincias (código y población)
-data_geo = get_data_geo()   #Datos de las coordenadas de las provincias
-
-
-
-# A partir del DataFrame original los NA en categoría de producto pasan a designase 'Sin Clasificar'
-
-datos_clean_or['productcat1'] = datos_clean_or['productcat1'].fillna('Sin clasificar')
-datos_clean_or['productcat2'] = datos_clean_or['productcat2'].fillna('Sin clasificar')
-datos_clean_or['productcat3'] = datos_clean_or['productcat3'].fillna('Sin clasificar')
-
-# DataFrame donde descartamos los datos sin código postal sólo para la representación del mapa y gráficas de provincias
-datos_clean_map = datos_clean_or[datos_clean_or['zp_sim'].notna()]
-
-
-
-# region MAPA
-# ---------------------------------
-#     MAPA
-# ---------------------------------
-
-#Orgenamos por código postal
-
-dat_1 = data_code.sort_values('CODIGO')
-dat_1['cod_prov'] = data_code['CODIGO'].astype(int).astype(str)
-dat_1['cod_prov'] = dat_1['cod_prov']
-data_all = dat_1.set_index('CODIGO')
-
-#Tipos de variables para el mapa 
 
 dicts = {"Balance total (€)": 'GAIN',
          "Balance relativo (€/100 mil hab.)": 'GAIN'}
@@ -270,8 +239,21 @@ def run_UI():
     #Página MAPA
     
     if page == 'Nivel geográfico y temporal':
-             
-    
+        
+        #Creación de los DataFrames
+        datos_clean_or = get_data_clean() #General con los datos de los tickets
+        data_code = get_data_prov() #Datos de las provincias (código y población)
+        data_geo = get_data_geo()   #Datos de las coordenadas de las provincias
+
+        # A partir del DataFrame original los NA en categoría de producto pasan a designase 'Sin Clasificar'
+
+        datos_clean_or['productcat1'] = datos_clean_or['productcat1'].fillna('Sin clasificar')
+        datos_clean_or['productcat2'] = datos_clean_or['productcat2'].fillna('Sin clasificar')
+        datos_clean_or['productcat3'] = datos_clean_or['productcat3'].fillna('Sin clasificar')
+
+        # DataFrame donde descartamos los datos sin código postal sólo para la representación del mapa y gráficas de provincias
+        datos_clean_map = datos_clean_or[datos_clean_or['zp_sim'].notna()]
+
         #Orgenamos por código postal
 
         dat_1 = data_code.sort_values('CODIGO')
@@ -563,6 +545,14 @@ def run_UI():
     elif page == 'Nivel de producto':
     
         get_data_clean.clear() #borrar caché
+        
+        #Creación de los DataFrames
+        datos_clean_or = get_data_clean() #General con los datos de los tickets
+        # A partir del DataFrame original los NA en categoría de producto pasan a designase 'Sin Clasificar'
+
+        datos_clean_or['productcat1'] = datos_clean_or['productcat1'].fillna('Sin clasificar')
+        datos_clean_or['productcat2'] = datos_clean_or['productcat2'].fillna('Sin clasificar')
+        datos_clean_or['productcat3'] = datos_clean_or['productcat3'].fillna('Sin clasificar')
         
         st.sidebar.write("""
             ## Nivel de producto
