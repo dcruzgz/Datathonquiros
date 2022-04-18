2903613983a7b8f41d91afbaa649b77fa4d14bfe
Daniel Cruz Garzón <102228842+dcruzgz@users.noreply.github.com>
2022-04-18 12:15:58 +0200

    Update mifarma_datathonquiros.py

--- mifarma_datathonquiros.py
+++ mifarma_datathonquiros.py
@@ -786,7 +786,7 @@ def run_UI():
                     alt.value("steelblue"),  # The positive color
                     alt.value("red")  # The negative color
                     ),
-                tooltip=[alt.Tooltip('Balance', title=Balance (€)'),"Descuento"]
+                tooltip=[alt.Tooltip('Balance', title='Balance (€)'),"Descuento"]
             )
             )
         st.altair_chart(chart, use_container_width=True)
